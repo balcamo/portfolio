633a7c3b1273379a5ff7c6ecf4189eb88d4e8665
Bethany Alcamo <balcamo@verawaterandpower.com>
2019-08-07 14:26:03 -0700

    web config update

---
+++ react/maintenance/build_prod_working/precache-manifest.721ff14ef6db663e6449a6cd3179de53.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40ba716a52b01846907cba066b583a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9316bfc828a9980c732",
+    "url": "/static/css/2.55e63a42.chunk.css"
+  },
+  {
+    "revision": "c4ce1c8c7ec3dc11cc53",
+    "url": "/static/css/main.27429b97.chunk.css"
+  },
+  {
+    "revision": "e9316bfc828a9980c732",
+    "url": "/static/js/2.d31eb9fb.chunk.js"
+  },
+  {
+    "revision": "c4ce1c8c7ec3dc11cc53",
+    "url": "/static/js/main.54a25241.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
